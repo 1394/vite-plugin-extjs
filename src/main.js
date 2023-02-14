9df831501b358951e14b72343f3c3ff3e9b7075d
Alexander Ivanov <ivanovayu@sigma-it.ru>
2023-02-14 16:05:36 +0300

    fix if empty mappings

--- src/main.js
+++ src/main.js
@@ -30,66 +30,70 @@ function realpath(path) {
     return nodePath.normalize(process.cwd() + '\\' + path).replace(/\\/g, '/');
 }
 
-export default (mappings) => ({
-    name: PLUGIN_NAME,
-    async transform(code, id) {
-        let ast;
-        const extend = [];
-        const uses = [];
-        const requires = [];
-        if (id.endsWith('.js')) {
-            ast = this.parse(code);
-            const existingImports = [];
-            walk(ast, {
-                enter: async node => {
-                    if (node.type === 'ImportDeclaration') {
-                        existingImports.push(realpath(node.source.value));
-                    }
-                    if (node.type === 'ExpressionStatement') {
-                        if (node.expression.callee.object.name === 'Ext') {
-                            // Ext.define
-                            if (node.expression.callee.property.name === 'define') {
-                                const props = node.expression.arguments[1].properties;
-                                props.forEach(prop => {
-                                    // extend
-                                    if (prop.key.name === 'extend') {
-                                        extend.push(prop.value.value);
-                                    }
-                                    // uses, requires, override
-                                    if (['uses', 'requires', 'override'].includes(prop.key.name)) {
-                                        if (prop.value.type === 'ArrayExpression') {
-                                            prop.value.elements.forEach(el => {
-                                                uses.push(el.value);
-                                            });
-                                        } else if (prop.value.type === 'Literal') {
-                                            uses.push(prop.value.value);
+export default (mappings) => {
+    return {
+        name: PLUGIN_NAME,
+        async transform(code, id) {
+            if(!mappings){
+                return ;
+            }
+            let ast;
+            const extend = [];
+            const uses = [];
+            const requires = [];
+            if (id.endsWith('.js')) {
+                ast = this.parse(code);
+                const existingImports = [];
+                walk(ast, {
+                    enter: async node => {
+                        if (node.type === 'ImportDeclaration') {
+                            existingImports.push(realpath(node.source.value));
+                        }
+                        if (node.type === 'ExpressionStatement') {
+                            if (node.expression.callee.object.name === 'Ext') {
+                                // Ext.define
+                                if (node.expression.callee.property.name === 'define') {
+                                    const props = node.expression.arguments[1].properties;
+                                    props.forEach(prop => {
+                                        // extend
+                                        if (prop.key.name === 'extend') {
+                                            extend.push(prop.value.value);
                                         }
-                                    }
-                                });
+                                        // uses, requires, override
+                                        if (['uses', 'requires', 'override'].includes(prop.key.name)) {
+                                            if (prop.value.type === 'ArrayExpression') {
+                                                prop.value.elements.forEach(el => {
+                                                    uses.push(el.value);
+                                                });
+                                            } else if (prop.value.type === 'Literal') {
+                                                uses.push(prop.value.value);
+                                            }
+                                        }
+                                    });
+                                }
                             }
                         }
                     }
-                }
-            })
-            const imports = [...extend, ...uses, ...requires];
-            let importStr = '';
-            console.log(existingImports, id);
-            for (const module of imports) {
-                const paths = await resolve(mappings, module);
-                paths.forEach(path => {
-                    if (path) {
-                        const realPath = realpath(path);
-                        if (!existingImports.includes(realPath) && !existingImports.includes(`${realPath}.js`)) {
-                            importStr += `import '${path}.js';\n`;
-                            existingImports.push(realPath);
+                })
+                const imports = [...extend, ...uses, ...requires];
+                let importStr = '';
+                for (const module of imports) {
+                    const paths = await resolve(mappings, module);
+                    paths.forEach(path => {
+                        if (path) {
+                            const realPath = realpath(path);
+                            if (!existingImports.includes(realPath) && !existingImports.includes(`${realPath}.js`)) {
+                                importStr += `import '${path}.js';\n`;
+                                existingImports.push(realPath);
+                            }
                         }
-                    }
-                });
+                    });
+                }
+                if (importStr.length) {
+                    code = `/*** <${PLUGIN_NAME}> ***/\n${importStr}/*** </${PLUGIN_NAME}> ***/\n\n${code}`;
+                }
             }
-            if (importStr.length) {
-                code = `/*** <${PLUGIN_NAME}> ***/\n${importStr}/*** </${PLUGIN_NAME}> ***/\n\n${code}`;
-            }
-        }
-        return {code, ast, map: null};
-    },
-})
+            return {code, ast, map: null};
+        },
+    }
+}
