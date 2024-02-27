d9250f4266e69e229adb27da929a4fdc0f790163
A. Ivanov <ivanovayu@sigma-it.ru>
2024-02-27 11:29:47 +0300

    check isIgnore in hmr

--- src/index.js
+++ src/index.js
@@ -188,6 +188,12 @@ const viteExtJS = ({
         },
         async handleHotUpdate(ctx) {
             const { file, server, modules } = ctx;
+            if (Path.isIgnore(file)) {
+                server.ws.send({
+                    type: 'full-reload',
+                });
+                return [];
+            }
             if (Path.isMatch(file, ['**/*.scss'])) {
                 Logger.warn('Recompiling theme css file.');
                 server.ws.send({
